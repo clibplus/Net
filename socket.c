7d80cb29e484e38cc25a25c90b7a0ac23e505d5d
algo1337 <bangod1337@gmail.com>
2024-10-12 19:35:09 +0000

    Socket lib

---
+++ socket.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "socket.h"
+
+Socket Create_TCP_Socket(Hostname_T *ip_t, String *ip, int port) {
+	Socket s = {
+		.IP 		= ip,
+		.Port 		= (!port ? 1337 : port),
+		.SockFD	 	= -1,
+		.BufferLen 	= 1024
+	};
+
+	s.SockFD = socket(AF_INET, SOCK_STREAM, 0);
+	if(s.SockFD == -1)
+		return (Socket){.IP = NULL, .Port = 0, .SockFD = 0, .BufferLen = 0};
+
+	bzero(&s.SockAddr, sizeof(s.SockAddr));
+	s.SockAddr.sin_family = AF_INET;
+	if(!ip)
+		inet_aton(ip->data, &s.SockAddr.sin_addr);
+
+	int reuse = 1;
+	if(setsockopt(s.SockFD, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuse, sizeof(reuse)) < 0)
+		return (Socket){.IP = NULL, .Port = 0, .SockFD = 0, .BufferLen = 0};
+
+	s.SockAddr.sin_port = htons(s.Port);
+	return s;
+}
+
+static int BindSocket(Socket *s) {
+	if(bind(s->SockFD, (struct sockaddr *)&s->SockAddr, sizeof(s->SockAddr)) != 0)
+		return 0;
+
+	return 1;
+}
+
+static int Connect(Socket *s) {
+	return connect(s->SockFD, (struct sockaddr *)&s->SockAddr, (socklen_t)sizeof(s->SockAddr));
+}
+
+static int Listen(Socket *s, int concurrent) {
+	if(!s || s->SockFD == -1)
+		return 0;
+
+	if(listen(s->SockFD, concurrent) != 0)
+		return 0;
+
+	return 1;
+}
+
+static Socket Accept(Socket *s) {
+	if(!s || s->SockFD == -1)
+		return ((Socket){.IP = NULL, .Port = 0, .SockFD = 0, .BufferLen = 0});
+
+	Socket client;
+
+	int len = sizeof(client.SockAddr);
+	client.SockFD = accept(s->SockFD, (struct sockaddr *)&client.SockAddr, &len);
+	
+	return client;
+}
+
+static void DestroySocket(String *s) {
+	if(s->IP)
+		free(s->IP);
+
+	close(s->SockFD);
+}
---
+++ socket.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <str.h>
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+typedef enum Hostname_T {
+	IPv4		= 0x038001,
+	IPv6		= 0x038002
+} Hostname_T;
+
+typedef struct Socket {
+	String 				*IP;
+	int					Port;
+	int					SockFD;
+	struct sockaddr_in	SockAddr;
+	int					BufferLen;
+
+	int					(*Bind)		(struct Socket *s);
+	int					(*Connect)	(struct Socket *s);
+	int					(*Listen)	(struct Socket *s);
+	Socket				(*Accept)	(struct Socket *s);
+	void				(*Destruct)	(struct Socket *s);
+} Socket;
+
+Socket Create_TCP_Socket(Hostname_T *ip_t, String *ip, int port);
+static int BindSocket(Socket *s);
+static int Connect(Socket *s);
+static int Listen(Socket *s, int concurrent);
+static Socket Accept(Socket *s);
+static void DestroySocket(String *s);
