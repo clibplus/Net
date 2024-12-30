8b85b04c21398b5326ae194568b3afe727bcb73b
Algorithms <142917637+Algo1337@users.noreply.github.com>
2024-12-30 03:01:41 -0600

    Websign moving to new repo

--- web.h
+++ web.h
@@ -21,74 +21,6 @@
 
 #include "request.h"
 
-/* Controls using HTML Tags */
-typedef enum ControlTag {
-    NO_TAG                              = 8490,
-
-    HTML_TAG                            = 8491,
-    HEAD_TAG                            = 8492,
-    BODY_TAG                            = 8493,
-
-    TITLE_TAG                           = 8494,
-    H1_TAG                              = 8495,
-    H2_TAG                              = 8496,
-    H3_TAG                              = 8497,
-    INPUT_TAG                           = 8498,
-    P_TAG                               = 8499,
-    DIV_TAG                             = 8500,
-    A_TAG                               = 8501,
-    PRE_TAG                             = 8502,
-    BUTTON_TAG                          = 8503,
-    FORM_TAG                            = 8504
-} ControlTag;
-
-typedef enum WJS_Value_T {
-    FORM_ELEMENTS                       = 9040,     // Provide a form ID <form id=""></form>
-    ELEMENT_IDS                         = 9041      // Provide an array of element IDs
-} WJS_Value_T;
-
-typedef enum WJS_Action_T {
-    NO_ACTION                           = 10930,
-    REDIRECT                            = 10931,
-    MSG_BEFORE_REDIRECT                 = 10932,
-    GET_RESULTS                         = 10933,
-    SPIN_UNTIL_RESULTS                  = 10934,
-    VERTICAL_BOOMERANG_UNTIL_RESULT     = 10935,
-    HORIZONTAL_BOOMERANG_UNTIL_RESULT   = 10936,
-} WJS_Action_T;
-
-typedef struct CSS {
-    char                *Class;
-    char                **Data;
-    int                 Selector;
-} CSS;
-
-typedef struct WJS {
-    WJS_Value_T         ValueType;      // OnClick Action Type
-    void                **Elements;     // Grab value of other elements using IDs
-    WJS_Action_T        Action;         // IF USING ANIMATION TAGS, AnimationID must be set.
-    char                *AnimationID;   // Element ID to the element you want animated
-    char                *ChangeID;      // Element ID to the element you want its value changed  
-} WJS;
-
-typedef struct Control {
-    void                *Parent;
-    ControlTag          Tag;        // ControlTag
-    char                *ID;
-    char                *Type;      // Type for <input> <button> <select> <script>
-    char                *Text;      // text for tags: <p> <h1> <h2> <h3>
-    char                *Class;     // class=""
-    char                *href;      // href for <a>
-    char                **CSS;      // CSS Function Generator for the tag <div style="CSS FUNCTION"></div>
-    char                *Data;      // For Any Other Data In the Opening Tag <div Data... > </div>
-    long                OnClick;    // Enable this to 1 and Use FormID and DisplayID
-    char                *OnClickJS; 
-    char                *FormID;
-    char                *DisplayID;
-    void                **SubControls;
-    long                SubControlCount;
-} Control;
-
 typedef struct WebRoute {
     char                *Name;          // Name of route
     char                *Path;          // Route
@@ -143,12 +75,6 @@ typedef struct cWR {
     
     void                (*Destruct)     (struct cWS *r);
 } cWR;
-
-typedef Control Textbox;
-typedef Control Button;
-typedef Control Div;
-
-extern void *HTML_TAGS[][2];
 
 // == [ Web Server Operation ] ==
 
@@ -237,57 +163,3 @@ void    DestroyCfg(WebServerConfig *cfg);
 //      | - > Route Destructor
 //
 void    DestroyRoute(WebRoute *r);
-
-// == [ Websign Template Generation Operations ] ==
-
-// == [ HTML / CSS Genetation ] ==
-
-//
-//
-//
-//
-char    *FindTag(Control *control);
-
-//
-//
-//
-//
-ControlTag FindTagType(const char *data);
-
-//
-//      | - > Construct a template using an array of Controls in HTML order and CSS styles
-//      | - > Returns 1 upon success or 0 upon failure
-//
-int     ConstructTemplate(WebRoute *route, Control **controls, CSS **styles);
-
-//
-//
-//
-//
-char    *ConstructCSS(WebRoute *route);
-
-//
-//
-//
-//
-String  ConstructParent(Control *p, int sub);
-
-// == [ WebJS Genetation ] ==
-
-//
-//
-//
-//
-String ConstructOnClickForm(Control *p);
-
-//
-//
-//
-//
-String  ConstructJS(WJS *js);
-
-String control2str(Control *p);
-String DumpControls(Control *controls, int nestingLevel);
-
-Control **process_html_line(const char *data);
-Control **ParseHTMLContent(const char *data);
